bb201c780586a718779f9d9059ffd7fa7060837c
Jarred Channer <jarredchanner@gmail.com>
2022-08-13 15:29:26 +0200

    Update Observer types to interfaces

--- src/types/observer.ts
+++ src/types/observer.ts
@@ -1,9 +1,9 @@
-export type Subscriber<T> = {
+export interface ISubscriber<T> {
   (data: T): void
 }
 
-export type Observer<T> = {
-  subscribe(subscriber: Subscriber<T>): void
-  unsubscribe(subscriber: Subscriber<T>): void
+export interface IObserver<T> {
+  subscribe(subscriber: ISubscriber<T>): void
+  unsubscribe(subscriber: ISubscriber<T>): void
   notify(data: T): void
 }
